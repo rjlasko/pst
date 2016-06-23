970b6d972dba5934bc2c1f1c20a14c9f49d8066f
Robert Lasko <robert.j.lasko@gmail.com>
2016-06-23 01:22:35 -0400

    Moved around the setting of 'ls' color variable declarations.

--- bash/bash_options.bash
+++ bash/bash_options.bash
@@ -10,8 +10,16 @@ if [ -z "$BASH_VERSION" ]; then
 	return
 fi
 
+##############################################
+# File + Directory creation permissions mask #
+##############################################
 
-# History Options
+# note that files are never created with their execution bits set, despite the mask
+umask 027
+
+###################
+# History Options #
+###################
 
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
@@ -30,8 +38,30 @@ HISTFILESIZE=1000000
 # export HISTIGNORE=$'[ \t]*:&:[fb]g:exit'
 # export HISTIGNORE=$'[ \t]*:&:[fb]g:exit:ls' # Ignore the ls command as well
 
+#############################
+# 'ls' Color Initialization #
+#############################
 
-# Shell Options
+
+case "$PST_OS" in
+	"darwin")
+		#if ( man ls | grep "BSD" ) ; then
+		export CLICOLOR=1
+		;;
+	*)
+		#elif ( man ls | grep "GNU" ) ; then
+		if [ -n "$(command -v dircolors 2>/dev/null)" ] ; then
+			([ -e ~/.dircolors] && eval "$(dircolors -b ~/.dircolors)") || (eval "$(dircolors -b)")
+		else
+			LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:';
+			export LS_COLORS
+		fi
+		;;
+esac
+
+#################
+# Shell Options #
+#################
 
 # See man bash for more options...
 
@@ -62,6 +92,3 @@ shopt -s checkwinsize
 # for example, cd /vr/lgo/apaache would find /var/log/apache
 # shopt -s cdspell
 
-# default file + directory creation mask
-# note that files are never created with their execution bits set, despite the mask
-umask 027
