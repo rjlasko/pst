a56c7520b3be4ae77359ae4c3e5237cb123b9345
Robert Lasko <robert.j.lasko@gmail.com>
2016-07-31 17:27:07 -0400

    Added file for commonly used docker commands

---
+++ dev/docker_alias.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+
+
+pst_debug_echo "$BASH_SOURCE"
+
+if [ -z "$(command -v docker 2>/dev/null)" ] ; then
+	echo "It appears that the docker executable 'docker' is not in the path!"
+	return
+fi
+
+alias docker='sudo docker'
+alias dkri="docker images"
+alias dkrps='docker ps -a'
+
+function dkrsh() {
+	docker exec -it $1 /bin/bash
+}
