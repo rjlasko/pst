60535dcd77d66a59ef188e8f51fc9ca85bbd4716
Robert Lasko <robert.j.lasko@gmail.com>
2016-11-07 11:45:53 -0500

    Leverage git-completion functions to augment existing git aliases.

--- dev/git/git-alias.sh
+++ dev/git/git-alias.sh
@@ -8,29 +8,47 @@ if [ -z "$(command -v git 2>/dev/null)" ] ; then
 	return
 fi
 
+if $(type __git_complete 2>/dev/null) ; then
+	# git-completion is in effect, so lets use it
+	__git_complete gitco _git_checkout
+	__git_complete gitd _git_diff
+	__git_complete gitg _git_log
+	__git_complete gitga _git_log
+	__git_complete gitgas _git_log
+	__git_complete gitl _git_log
+	__git_complete gitmrg _git_merge
+	__git_complete gitmrgsafe _git_merge
+	__git_complete gitp _git_pull
+	__git_complete gitr _git_remote
+fi
 
-alias gitl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gitg='git log --graph --oneline --decorate'
-alias gitga='git log --all --graph --decorate --oneline' # --simplify-by-decoration'
-alias gitgas='git log --all --graph --decorate --oneline --simplify-by-decoration'
+# but we still need the aliases, because completion is not enough
 alias gitb='git branch'
 alias gitba='git branch -a'
-alias gitstat='git status'
-alias gitremote='git remote -v'
+alias gitco='git checkout'
+alias gitd='git diff'
+alias gitg='git log --graph --oneline --decorate'
+alias gitga='git log --graph --oneline --decorate --all'
+alias gitgas='git log --graph --oneline --decorate --all --simplify-by-decoration'
+alias gitl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gitmast='git checkout master'
 alias gitmastf='git checkout -f master'
-
-alias gitpull='git pull --rebase'
-alias gitrpull='git pull --rebase --recurse-submodules'
-#alias gitrpull='git submodule foreach --recursive git pull'
-alias gitrclone='git clone --recursive'
-alias gitrupdt='git submodule update --init --recursive'
-
-alias gitmergesafe='git merge --no-commit --no-ff'
+alias gitmrg='git merge --no-commit'
+alias gitmrgsafe='git merge --no-commit --no-ff'
+alias gitp='git pull'
+alias gitr='git remote -v'
+alias gitst='git status'
 alias gitwipe='git clean -dxf'
-alias gitalias='alias | grep "git"'
 
-function gitcohash() {
+#alias gitpull='git pull --rebase'
+#alias gitrpull='git pull --rebase --recurse-submodules'
+#alias gitrclone='git clone --recursive'
+#alias gitrpull='git submodule foreach --recursive git pull'
+#alias gitrupdt='git submodule update --init --recursive'
+
+#alias gitalias='alias | grep "git"'
+
+function gitcoh() {
 	git checkout $(git rev-parse HEAD)
 }
 
