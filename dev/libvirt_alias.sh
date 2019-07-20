118547f63ec4ab0a283e88d1e50b631555f64c25
Robert Lasko <robert.j.lasko@gmail.com>
2019-07-19 23:57:20 -0400

    created new alias file for libvirt

---
+++ dev/libvirt_alias.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+pst_debug_echo "$BASH_SOURCE"
+
+if [ -n "$(type -t virsh)" ] ; then
+	export LIBVIRT_DEFAULT_URI='qemu:///system'
+	virsh list --all
+else
+	echo "It appears that the libvirt executable 'virsh' is not in the path!"
+	return
+fi
