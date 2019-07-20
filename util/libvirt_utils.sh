a08f38be3c3d4a7a348656f2920fe8b147e87140
Robert Lasko <robert.j.lasko@gmail.com>
2019-07-20 00:20:49 -0400

    fix missing quote

--- util/libvirt_utils.sh
+++ util/libvirt_utils.sh
@@ -4,7 +4,7 @@
 pst_debug_echo "$BASH_SOURCE"
 
 if [ -n "$(type -t virsh)" ] ; then
-	echo "<<<<<<<<<< Virtual Machine Status >>>>>>>>>>
+	echo "<<<<<<<<<< Virtual Machine Status >>>>>>>>>>"
 	export LIBVIRT_DEFAULT_URI='qemu:///system'
 	virsh list --all
 else
