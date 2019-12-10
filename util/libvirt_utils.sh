96fccefbd5ee3f8e7642bebc970d90e8b78d2ec3
Robert Lasko <robert.j.lasko@gmail.com>
2019-12-10 01:02:36 -0500

    added useful aliases and function to libvirt_utils.sh

--- util/libvirt_utils.sh
+++ util/libvirt_utils.sh
@@ -11,3 +11,14 @@ else
 	echo "It appears that the libvirt executable 'virsh' is not in the path!"
 	return
 fi
+
+alias vmls="virsh list"
+alias vmstart="virsh start"
+alias vmdown="virsh shutdown"
+alias vmstop="virsh destroy"
+
+function vmrm() {
+	local vm_image_path="$(virsh vol-list default | grep $1 | awk '{print $2}')"
+	virsh undefine $1
+	rm -f "${vm_image_path}"
+}
